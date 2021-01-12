7566c6e814078168fe355f6e730fc227f428e36e
prajeeshag <prajeeshag@gmail.com>
2021-01-12 15:59:07 +0530

    Updated

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+dir="$(dirname "$0")"
+cd "$dir" || exit 1
 mkdir -p archive
 echo "archiving" > .archive_lock
 (python ./manage.py archive && echo "done" > .archive_lock) || echo "Error" > .archive_lock
