c6da00e49a1128cc1d4ca367f5241eeec37c23d4
prajeeshag <prajeeshag@gmail.com>
2021-01-15 18:49:28 +0530

    Updated

--- VFFreg/settings.py
+++ VFFreg/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#(jv6__=f7*1hb_058qrh!e@ci7)g3qr)2w_v!xs7c2s2830br'
+SECRET_KEY = "#(jv6__=f7*1hb_058qrh!e@ci7)g3qr)2w_v!xs7c2s2830br"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,74 +33,74 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
-    'django.contrib.staticfiles',
-    'django.forms',
-    'sorl.thumbnail',
-    'crispy_forms',
-    'bootstrap4',
-    'hijack',
-    'compat',
-    'django_unused_media',
-    'users.apps.UsersConfig',
-    'registration.apps.RegistrationConfig',
-    'django_cleanup.apps.CleanupConfig',
-    'widget_tweaks',
-    'myapp',
-    'extra_views',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "django.contrib.staticfiles",
+    "django.forms",
+    "sorl.thumbnail",
+    "crispy_forms",
+    "bootstrap4",
+    "hijack",
+    "compat",
+    "django_unused_media",
+    "users.apps.UsersConfig",
+    "registration.apps.RegistrationConfig",
+    "django_cleanup.apps.CleanupConfig",
+    "widget_tweaks",
+    "myapp",
+    "extra_views",
 ]
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'VFFreg.urls'
+ROOT_URLCONF = "VFFreg.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'VFFreg.wsgi.application'
+WSGI_APPLICATION = "VFFreg.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -110,16 +110,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -134,9 +134,9 @@ ACCOUNT_SESSION_REMEMBER = True
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -148,32 +148,42 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 def location(x):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), x)
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "frontend/static"),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-LOGIN_REDIRECT_URL = '/home'
-LOGOUT_REDIRECT_URL = 'accounts/login'
+LOGIN_REDIRECT_URL = "/home"
+LOGOUT_REDIRECT_URL = "accounts/login"
 
-FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
+FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 BOOTSTRAP4 = {
-    'include_jquery': True,
-    'javascript_in_head': True,
+    "include_jquery": True,
+    "javascript_in_head": True,
 }
 HIJACK_USE_BOOTSTRAP = True
 
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = "users.User"
 AUTHENTICATION_BACKENDS = (
-    'users.backends.AuthBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "users.backends.AuthBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 )
 
-THUMBNAIL_ENGINE = 'myapp.thumbnail.pil_engine.Engine'
+THUMBNAIL_ENGINE = "myapp.thumbnail.pil_engine.Engine"
+
+if DEBUG:
+    INSTALLED_APPS.append('sass_processor',)
+    SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'frontend')
+    SASS_PROCESSOR_AUTO_INCLUDE = False
+    SASS_PROCESSOR_INCLUDE_FILE_PATTERN = r'^.+\.scss$'
