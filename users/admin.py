f16569ca4b4341e33966121bb5f3fa81e70d38e8
prajeeshag <prajeeshag@gmail.com>
2021-01-27 15:34:54 +0530

    Updated

--- users/admin.py
+++ users/admin.py
@@ -5,3 +5,5 @@ from . import models
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.ClubProfile)
+admin.site.register(models.PhoneNumber)
+admin.site.register(models.ProfilePicture)
