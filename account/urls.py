a3af6ceb212324313126b9cf7a74c3fa6b445c56
prajeeshag <prajeeshag@gmail.com>
2021-01-27 20:14:45 +0530

    Password reset

--- account/urls.py
+++ account/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='account_login'),
     path('signup/', views.SignupView.as_view(), name='account_signup'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('passwdreset/', views.LoginView.as_view(),
+    path('passwdreset/', views.PasswordResetView.as_view(),
          name='account_reset_password'),
     path('login/', views.LoginView.as_view(), name='account_change_password'),
     path('logout/', LogoutView.as_view(), name='account_logout'),
--- account/views.py
+++ account/views.py
@@ -4,11 +4,13 @@ from django.utils.translation import ugettext_lazy as _
 
 from django.contrib.auth.views import LoginView as LoginViewCore
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib import messages
 
 from formtools.wizard.views import SessionWizardView
+
 
 from . import forms
 from users.models import PhoneNumber, PlayerProfile, User
@@ -18,6 +20,44 @@ class LoginView(LoginViewCore):
     template_name = 'account/login.html'
     redirect_authenticated_user = True
     authentication_form = forms.LoginForm
+
+
+class PasswordResetView(SessionWizardView):
+    template_name = 'account/password_reset.html'
+    form_list = [
+        forms.PassWordResetStep1,
+        forms.PassWordResetStep2,
+        forms.PassWordResetStep3,
+    ]
+
+    def get_form_kwargs(self, step=None):
+        kwargs = super().get_form_kwargs(step)
+        if step == '1':
+            kwargs['request'] = self.request
+            data = self.get_cleaned_data_for_step('0')
+            kwargs['phone_number'] = data.get('phone_number')
+        return kwargs
+
+    # def get(self, request, *args, **kwargs):
+        # try:
+        # return self.render(self.get_form())
+        # except KeyError:
+        # return super().get(request, *args, **kwargs)
+
+    def done(self, form_list, **kwargs):
+        UserModel = get_user_model()
+        forms = [form for form in form_list]  # get all forms
+        password = forms[2].cleaned_data.get('password1')
+        phone_number = forms[0].cleaned_data.get('phone_number')
+        user = UserModel.objects.filter(
+            phone_number__number=phone_number).distinct().first()
+        user.set_password(password)
+        user.save()
+
+        messages.add_message(
+            self.request, messages.SUCCESS,
+            _('Password reset successful, login to continue..'))
+        return redirect('login')
 
 
 class SignupView(SessionWizardView):
