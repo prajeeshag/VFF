45f16b6b8eab080e9765ee003f93daf3d1c05e01
prajeeshag <prajeeshag@gmail.com>
2021-02-01 03:55:15 +0530

    Updated

--- core/mixins.py
+++ core/mixins.py
@@ -28,10 +28,11 @@ class BackUrlMixin:
 class RedirectToPreviousMixin(BackUrlMixin):
 
     def get_success_url(self):
-        url = self.request.session.get('previous_page', None)
+        url = None
+        if self.request.method == 'POST':
+            url = self.request.POST.get('redirect_url', None)
         if url is None:
-            if self.request.method == 'POST':
-                url = self.request.POST.get('redirect_url', None)
+            url = self.request.session.get('previous_page', None)
         if url is None:
             url = self.request.META.get('HTTP_REFERER', self.default_redirect)
         return url
