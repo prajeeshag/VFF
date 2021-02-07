28040791641ad3fcb189fbfbd1c41e015eec85ce
prajeeshag <prajeeshag@gmail.com>
2021-02-07 14:36:34 +0530

    Updated

--- verification/signals.py
+++ verification/signals.py
@@ -4,18 +4,21 @@ from guardian.shortcuts import assign_perm, remove_perm
 
 from users.models import PlayerProfile, Document
 from .models import Verification
+from core.utils import disable_for_loaddata
 
 
 @receiver(post_save, sender=PlayerProfile)
+@disable_for_loaddata
 def create_verification(sender, instance, created, **kwargs):
     if not hasattr(instance, 'verification'):
         Verification.objects.create(profile=PlayerProfile)
 
 
 @receiver(pre_save, sender=PlayerProfile)
-def create_review_submit1(sender, instance, created, **kwargs):
+@disable_for_loaddata
+def create_review_submit1(sender, instance, **kwargs):
     if instance.pk:
-        previous = PlayerProfile.objects.get(pk=instance.pk)
+        previous = sender.objects.get(pk=instance.pk)
         if previous.dob != instance.dob:
             if instance.verification.need_review():
                 instance.verification.review_submitted = True
@@ -24,9 +27,10 @@ def create_review_submit1(sender, instance, created, **kwargs):
 
 
 @receiver(pre_save, sender=Document)
-def create_review_submit2(sender, instance, created, **kwargs):
+@disable_for_loaddata
+def create_review_submit2(sender, instance, **kwargs):
     if instance.pk:
-        previous = PlayerProfile.objects.get(pk=instance.pk)
+        previous = sender.objects.get(pk=instance.pk)
         if previous.image != instance.image:
             try:
                 verification = instance.collection.playerprofile.verification
