7634e9177c1fb0bfb618bbcd578723b197f374de
prajeeshag <prajeeshag@gmail.com>
2021-03-19 01:50:52 +0530

    updated

--- static/match/sub_v1.js
+++ static/match/sub_v1.js
@@ -15,32 +15,46 @@ $(window).on('load', function() {
 	$('button[name="save"]').attr('disabled',true)
 	$('button[name="substitute"]').attr('disabled',true)
 	
-	$('input[name="player_out"]').change(function() {
+	$('input[name=player_out][type=radio]').change(function() {
 
 		side = $(this).attr('side')
 		var nU19 = 0 
 		var nU21 = 0 
 
-		$('input[name="player_out"][side='+side+']').each(function() {
+		$('input[name=player_out][type=radio][side='+side+']').each(function() {
 			el = $(this)
 			age = el.data('player-age')
 			if (age <= 19) { nU19 += 1 } 
 			if (age <= 21) { nU21 += 1 }
 		});
 
-		$("input[name=player_out][side="+side+"]").closest('div').css({"border-style":""})
+		$('input[name=player_out][type=hidden][side='+side+']').each(function() {
+			el = $(this)
+			age = el.data('player-age')
+			if (age <= 19) { nU19 -= 1 } 
+			if (age <= 21) { nU21 -= 1 }
+		});
 
-		$(this).closest('div').css({"border-style":"solid", "border-color":"yellow"})
+		$('input[name=player_in][type=hidden][side='+side+']').each(function() {
+			el = $(this)
+			age = el.data('player-age')
+			if (age <= 19) { nU19 += 1 } 
+			if (age <= 21) { nU21 += 1 }
+		});
+
+		$("input[name=player_out][type=radio][side="+side+"]").closest('div').css({"border-style":""})
+
+		$(this).closest('div').css({"border-style":"solid", "border-color":"green"})
 		age = $(this).data('player-age')
 
-		$('input[name=player_in][side='+side+']').each(function() {
+		$('input[name=player_in][type=radio][side='+side+']').each(function() {
 			$(this).closest('div').css({"border-style":""})
 			$(this).attr('disabled',false)
 			$(this).prop('checked', false);
 		});
 
 		if (age<=19 && nU19-1 < NU19S){
-			$('input[name=player_in][side='+side+']').each(function() {
+			$('input[name=player_in][type=radio][side='+side+']').each(function() {
 				el = $(this)
 				age = el.data('player-age')
 				if (age > 19) {
@@ -50,7 +64,7 @@ $(window).on('load', function() {
 			});
 		}
 		else if (age<=21 && nU21-1 < NU21S) {
-			$('input[name=player_in][side='+side+']').each(function() {
+			$('input[name=player_in][type=radio][side='+side+']').each(function() {
 				el = $(this)
 				age = el.data('player-age')
 				if (age > 21) {
@@ -66,15 +80,14 @@ $(window).on('load', function() {
 		$('button[name="substitute"][side='+side+']').attr('disabled',true)
 	});
 
-	$('input[name=player_in]').change(function() {
+	$('input[name=player_in][type=radio]').change(function() {
 		side = $(this).attr('side')
-		$('input[name=player_in][side='+side+']').filter("input:enabled").closest('div').css({"border-style":""})
+		$('input[name=player_in][type=radio][side='+side+']').filter("input:enabled").closest('div').css({"border-style":""})
 		$(this).closest('div').css({"border-style":"solid", "border-color":"green"})
 		if ($('input[name=player_out]:checked').val() && $('input[name=player_in]:checked').val()) {
 			$('button[name="save"][side='+side+']').attr('disabled',false)
 			$('button[name="substitute"][side='+side+']').attr('disabled',false)
 		};
 	});
-
 });
 
